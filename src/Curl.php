2805f0c9534ba8f10625666424ebc7baefb7dc15
Nick Shtansky <n.shtanschi@unifun.com>
2018-08-17 07:48:02 +0300

    Initial commit

---
+++ src/Curl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 16.08.2018
+ * Time: 20:40
+ */
+
+namespace Unifun;
+
+
+class Curl
+{
+    public function SayHi()
+    {
+      echo ("Hi!".PHP_EOL);
+    }
+}
